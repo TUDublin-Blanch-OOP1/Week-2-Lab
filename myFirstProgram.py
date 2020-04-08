c5bc8833f15c52d4e17ce8a94ceef23ce0714cff
Brian6262 <40770236+Brian6262@users.noreply.github.com>
2020-04-08 16:59:43 +0100

    Add files via upload

---
+++ myFirstProgram.py
@@ -0,0 +1,22 @@
+# myFirstProgram.py
+# John Doe
+# 1st June 2034
+
+name = "Danny Boyce"
+address1 = "TU - Dublin Blanchardstown Campus"
+address2 = "Blanchardstown"
+postCode = "D15"
+
+apples = 36
+price = 0.78
+totalCost = apples * price
+
+print(name)
+print(address1)
+print(address2)
+print(postCode)
+
+print()
+print()
+
+print("The cost of apples is",totalCost)
